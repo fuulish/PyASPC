6bc15b96f86796d1e17fc1a0465f9385d24fd0ed
Frank Uhlig <uhlig.frank@gmail.com>
2017-07-24 22:45:09 +0200

    bumping version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='aspc',
-      version='0.1',
+      version='0.2',
       description='Always-stable predictor corrector algorithm',
       url='http://github.com/fuulish/PyASPC',
       author='Frank Uhlig',
